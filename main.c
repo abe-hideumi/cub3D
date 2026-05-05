b4ab54e896e2ef84a3aa6148f489dc8616aa242a
pipapo888 <kazuya2002uk@gmail.com>
2026-05-05 21:24:40 +0900

    file サイズすべて管理をやめる

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "map.h"
 
 int main(int ac, char *av[])
 {
 	if (ac != 2)
 		return (printf("jjj\n"), 1);
-	parse_map(av[1]);
+	t_info info;
+
+	parse_map(av[1], info);
 
 
 
--- parse_map.c
+++ parse_map.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include "map.h"
 
-typedef struct s_map
+void	put_error(void)
 {
-	char		**map;
-	int			player_direction;
-	size_t		width;
-	size_t		height;
-}				t_map;
-
+	ft_putstr_fd("Error\n", 2);
+	exit(1);
+}
 
 void	check_extenstion(char *map_name)
 {
@@ -15,15 +14,58 @@ void	check_extenstion(char *map_name)
 
 	len = ft_strlen(map_name);
 	if (len < 5 || ft_strncmp(".cub", map_name + len - 4, 4))
-		put_error_free_close(-1, NULL, "Invalid file extension. Must be .cub\n",
-			NULL);
+		printf("Invalid file extension. Must be .cub\n");
 }
 
-void parse_map(char *file)
+void read_file(char *file, t_info *info)
 {
+	int fd;
+	char *line;
+	size_t len;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		put_error();
+	line = get_next_line(fd);
+	while (line)
+	{
+		len = 0;
+		while (line[len] && line[len] != '\0')
+			len++;
+		if (info->file_max_len < len)
+			info->file_max_len = len;
+		info->file_height++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+}
+
+void parse_config(char *file, t_info *info)
+{
+	info->file_max_len = 0;
+	info->file_height = 0;
 	check_extenstion(file);
+	read_file(file, info);
+	
+
+
+
+}
+
+
+// 仮置きメインファイル
+int main(int ac, char *av[])
+{
+	if (ac != 2)
+		return (printf("jjj\n"), 1);
+	t_info info;
+
+	parse_config(av[1], info);
 
 
 
 
+
+	return (0);
 }
