931fa3a2df1b679774368d23ceda29a4dd27cc72
pipapo888 <kazuya2002uk@gmail.com>
2026-05-23 15:47:21 +0900

    parse_cub　ファイル分割　ファイル構成変更

--- map.h
+++ map.h
@@ -1,6 +1,14 @@
 # ifndef MAP_H
 #define MAP_H
 
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include "libft/libft.h"
+#include "get_next_line/get_next_line.h"
+
 typedef enum e_char_type {
 	CHAR_INVALID,
 	CHAR_PLAYER,
@@ -42,5 +50,26 @@ typedef struct s_info
 	char		player_dir;
 	t_player    player;
 }		t_info;
+
+// parse_cub_utils.c
+void	*ft_realloc(void *ptr, size_t new_size);
+bool	ft_is_space(char c);
+void	put_error(char *msg);
+
+// parse_cub_file.c
+void	parse_cub_file(char *file, t_info *info);
+
+// parse_texture.c
+void	parse_texture_img(char **field, char *line);
+void	parse_texture_color(int *field, char *line);
+
+// read_file.c
+void	read_file(char *file, t_info *info);
+
+// validate_map_chars.c
+void	validate_map_chars(t_info *info);
+
+// validate_map_enclosed.c
+void	validate_map_enclosed(t_info *info);
 
 #endif
